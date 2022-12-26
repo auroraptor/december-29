ac8b18c01f127a16c601bfd2f4f50f993583f55d
auroraptor <interestingbutno@yandex.ru>
2022-12-26 22:05:30 +0300

    feat: 💬 chat layout

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 import styles from "./styles.module.scss";
 
 function Profile() {
-
-    return <div className={styles.container}>
-        <h1>Страница для творчества</h1>
+  return (
+    <div className={styles.container}>
+      <section id="main" className={styles.main}>
+        <article id="card" className={styles.card}>
+          Card
+        </article>
+        <section id="messages-list" className={styles.messages_list}>
+          Messages list
+        </section>
+        <section id="new-message" className={styles.new_message}>
+          New message
+        </section>
+      </section>
     </div>
+  );
 }
 
 export default Profile;
