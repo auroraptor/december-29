0e9f6176eb4d1600c0b2b92ff1057e4c851e339b
auroraptor <interestingbutno@yandex.ru>
2022-12-30 16:05:49 +0300

    feat: 🫲 submit

--- src/components/AddMessage.jsx
+++ src/components/AddMessage.jsx
@@ -1,42 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import styles from "../pages/Profile/styles.module.scss";
 
-
 const AddMessage = (props) => {
-  let input
+  let input;
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    props?.dispatch(input.value, "Me");
+    input.value = "";
+  };
 
   return (
-    <section id="new-message" className={styles.new_message}>
+    <form
+      id="new-message"
+      className={styles.new_message}
+      onSubmit={handleSubmit}
+    >
       <input
-        onKeyPress={(e) => {
-        if (e.key === 'Enter') {
-          props?.dispatch(input.value, 'Me')
-          input.value = ''
-        }
-      }}
         type="text"
         ref={(node) => {
-        input = node
-      }}
-      className={styles.input}
-      placeholder={"Введите текст сообщения"}
-      />
-      {/* <button className={styles.button} onClick={
-        (e) => {
-            props?.dispatch(input.value, 'Me')
-            input.value = ''
+          input = node;
         }}
-          ref={(node) => {
-          input = node
-        }
-      }>Отправить</button> */}
-    </section>
-  )
-}
+        className={styles.input}
+        placeholder={"Введите текст сообщения"}
+      />
+      <button type="submit" className={styles.button}>
+        Отправить
+      </button>
+    </form>
+  );
+};
 
 AddMessage.propTypes = {
-  dispatch: PropTypes.func.isRequired
-}
+  dispatch: PropTypes.func.isRequired,
+};
 
 export default AddMessage;
