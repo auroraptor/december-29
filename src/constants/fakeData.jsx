3bb4f3391c629066e24118040ac15db2a0a509c2
auroraptor <interestingbutno@yandex.ru>
2023-03-16 17:06:42 +0300

    feat: 👾 real fake data

--- src/constants/fakeData.jsx
+++ src/constants/fakeData.jsx
@@ -1,15 +1,11 @@
 export const fakeMessages = [
-    'This Pokémon is nocturnal. Even in total darkness, its large eyes can spot its prey clearly!Wynaut gather on moonlit nights to play by squeezing up against each other. By being squeezed, this Pokémon gains endurance and is trained to dole out powerful counterattacks.Wherever Slaking live, rings of over a yard in diameter appear in grassy fields. They are made by the Pokémon as it eats all the grass within reach while lying prone on the ground.',
-    'Overjoyed at finally being able to fly',
-    'This Pokémon stores up feelings of happiness inside its shell, then shares them with others.When visiting a junkyard, you may catch sight of it having an intense fight with Murkrow over shiny objects.',
-    'Rhyhorn’s brain is very small. It is so dense, while on a run it forgets why it started running in the first place. It apparently remembers sometimes if it demolishes something.Entei embodies the passion of magma.',
-    'As its energy, Togepi uses the positive emotions of compassion and pleasure exuded by people and Pokémon',
-    ' It apparently remembers sometimes if it demolishes something.Entei embodies the passion of magma. This Pokémon is thought to have been born in the eruption of a volcano.',
-    'Wherever Slaking live, rings of over a yard in diameter appear in grassy fields. They are made by the Pokémon as it eats all the grass within reach while lying prone on the ground.',
-    'The Pokémon are biding their time, waiting for the traveler to tire and become incapable of moving.',
-    '🤝',
+    'People say it\'s enough and I got my point across ... the point isn\'t across until we cross the point',
+    '🌙',
+    'Two years ago we had 50 million people subscribed to music streaming services around the world. Today we have 400 million.',
+    '👾',
+    'Just stop lying about shit. Just stop lying.'
 ]
 
-const date = new Date('30 December 2022 13:26');
+const date = new Date('14 March 2023 13:26');
 
 export const fakeDate = date.toLocaleDateString().replace(/\//g, '.') + ' ' + date.toLocaleTimeString().slice(0, -3);
